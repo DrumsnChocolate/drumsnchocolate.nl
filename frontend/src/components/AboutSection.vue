b3a0dca354cf2408ca9afc015b7fb4c406984957
DrumsnChocolate <matbronk@gmail.com>
2023-10-22 00:13:35 +0200

    include a more complete background desc

--- frontend/src/components/AboutSection.vue
+++ frontend/src/components/AboutSection.vue
@@ -2,7 +2,7 @@
   <div id="about" class="about-section bg-platinum">
     <h2>Hi, I'm Matteo.</h2>
     <h1>I develop AI and the Web;</h1>
-    With a background in software engineering and mathematics.
+    With a background in Data Science, Software Engineering, and Mathematics.
   </div>
 </template>
 
