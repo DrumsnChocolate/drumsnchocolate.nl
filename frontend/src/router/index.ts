8e68baa193369619e752d71c34c4e7307dc48279
DrumsnChocolate <matbronk@gmail.com>
2024-04-13 22:16:54 +0200

    remove failed attempt at redirect

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import EmptyView from '../views/EmptyView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,14 +8,6 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
-    },
-    {
-      path: '/allesbehalvepop',
-      beforeEnter: () => {
-        location.href =
-          'https://open.spotify.com/playlist/5JoczxodeFm4vwLcNPgfvZ?si=d6401f1679564a8d&pt=76293441e5e87d2daaad52a7bd8a7e2f'
-      },
-      component: EmptyView
     }
   ]
 })
