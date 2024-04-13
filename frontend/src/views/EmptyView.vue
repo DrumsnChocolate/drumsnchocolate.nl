69929e30d961b02d9ec59e4634ec0383aabdf871
DrumsnChocolate <matbronk@gmail.com>
2024-04-13 21:41:19 +0200

    add redirect to spotify url I need to update every once in a while

---
+++ frontend/src/views/EmptyView.vue
@@ -0,0 +1,7 @@
+<template>
+  Automatic redirect to
+  <a
+    href="https://open.spotify.com/playlist/5JoczxodeFm4vwLcNPgfvZ?si=d6401f1679564a8d&pt=76293441e5e87d2daaad52a7bd8a7e2f"
+    >https://open.spotify.com/playlist/5JoczxodeFm4vwLcNPgfvZ?si=d6401f1679564a8d&pt=76293441e5e87d2daaad52a7bd8a7e2f</a
+  >
+</template>
